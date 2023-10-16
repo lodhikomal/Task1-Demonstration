8a04cefc6015e450944f8d1251fe449d7bacc707
lodhikomal <komallodhi720@gmail.com>
2023-10-16 19:25:27 +0530

    f

--- src/component/GetRequest.js
+++ src/component/GetRequest.js
@@ -2,26 +2,50 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 function GetRequest() {
   const [data, setData] = useState([]);
-  //   const [checked, setChecked] = useState("");
+  //   const [store, setStore] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://13.231.17.170:8080/getTableFields?tablename=tbuser")
       .then((res) => {
-        console.log(res.data.data, "get");
+        // console.log(res.data.data, "get");
         setData(res.data.data);
       });
   }, []);
 
+  //   console.log(store, "finally");
+  const handleChange = (e, index) => {
+    console.log(e, index, "test");
+    // setData([e.target.value]);
+    setData((prev) => {
+      return [
+        ...prev.slice(0, index),
+        {
+          ...prev[index],
+          Value: e.target.value,
+        },
+        ...prev.slice(index + 1),
+      ];
+    });
+  };
+
+  console.log(data, " check");
+
   return (
     <div>
-      {data.map((post) => {
+      {data.map((post, index) => {
         const { Field, Type } = post;
         return (
           <div>
             <label>{Field}=</label>
-            <input type="text" placeholder={Type} />
+            <input
+              type="text"
+              placeholder=""
+              value={post?.Value || ""}
+              onChange={(e) => handleChange(e, index)}
+            />
             <br></br>
-            <input type="checkbox" />
+            {/* <input type="checkbox" /> */}
           </div>
         );
       })}
